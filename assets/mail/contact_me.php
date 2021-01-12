c212759c2a8f5f0c436e45d06af89be1d3b0e694
Matthew Gould (Desktop) <49723415+Matthew-E-Gould@users.noreply.github.com>
2021-01-12 12:32:31 +0000

    tweaks

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,8 +1,8 @@
 <?php
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+    http_response_code(500);
+    exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -11,12 +11,18 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "matt.ed.gould@gmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "matt.ed.gould@gmail.com"; // Where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@matthew-e-gould.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+$mail=mail($to, "Subject: $email_subject",$message );
+if($mail){
+    echo "Thank you for using our mail form";
+    http_response_code(200);
+}else{
+    echo "Mail sending failed.";
+    http_response_code(500);
+}
 ?>
