600f6e056ac58c9147e455417556e76b84367ce3
Matthew G (Desktop) <49723415+Matthew-E-Gould@users.noreply.github.com>
2021-09-08 08:36:43 +0100

    tweaks

--- funny/generateIndex.py
+++ funny/generateIndex.py
@@ -4,12 +4,13 @@ print('running!')
 listDir = os.listdir('funny')
 #print(listDir)
 
-htmlContent = ""
-
 print('generating file!')
 
+htmlContent = "<!DOCTYPE html><html><head><title>The funny pages</title></head><body>"
 for dir in listDir:
     htmlContent = htmlContent + "<a href='/"+ dir +"'>"+ dir +"</a>"
+
+htmlContent = htmlContent + "</body></html>"
 
 print("saving file!")
 
