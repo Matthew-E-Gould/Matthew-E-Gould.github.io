3424e7f7924f041d8aa43e9b2a4775e353851876
Matthew G (Desktop) <49723415+Matthew-E-Gould@users.noreply.github.com>
2021-09-08 09:21:00 +0100

    tweak

--- funny/generateIndex.py
+++ funny/generateIndex.py
@@ -8,7 +8,7 @@ listDir = os.listdir('funny')
 print('generating file!')
 
 htmlContent = "<!DOCTYPE html><html><head><title>The funny pages</title></head><body>\n"
-htmlContent = "<h1>Looks like you found the funny pages!</h1><br>\n"
+htmlContent = htmlContent + "<h1>Looks like you found the funny pages!</h1><br>\n"
 for dir in listDir:
     htmlContent = htmlContent + "<a href='./"+ dir +"'>"+ dir +"</a><br>\n"
 
