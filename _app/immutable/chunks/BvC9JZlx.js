222805314ccf6848b46e28da3cfdba5b0e5ec376
Matthew Gould <49723415+Matthew-E-Gould@users.noreply.github.com>
2025-07-15 22:11:16 +0100

    easter egg addition

---
+++ _app/immutable/chunks/BvC9JZlx.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1k5s4cz?.base??"",a=globalThis.__sveltekit_1k5s4cz?.assets??s;export{a,s as b};
--- _app/immutable/chunks/q0381wQi.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1iprbbs?.base??"",a=globalThis.__sveltekit_1iprbbs?.assets??s;export{a,s as b};
